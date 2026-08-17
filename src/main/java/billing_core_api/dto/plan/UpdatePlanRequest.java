fc2b070444ce6fa96853eb9bc966201b70bbe60f
Caio Eduardo <caiotech02@gmail.com>
2026-08-17 14:56:01 -0300

    refactor Plan

---
+++ src/main/java/billing_core_api/dto/plan/UpdatePlanRequest.java
@@ -0,0 +1,20 @@
+package billing_core_api.dto.plan;
+
+import billing_core_api.domain.subscription.BillingCycle;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record UpdatePlanRequest(@NotBlank
+                                String newNamePlan,
+                                @NotNull
+                                @DecimalMin(value = "0.01")
+                                BigDecimal newPrice,
+                                @NotBlank
+                                String newDescription,
+                                @NotNull
+                                BillingCycle newBillingCycle
+                                ) {
+}
